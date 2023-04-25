abf5af89f763b7dc8ca7420961544b0eadc10404
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-04-25 12:26:56 +0000

    [fix]Login後の遷移先修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,7 +26,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
   
   def after_sign_in_path_for(resource)
-    wines_myindex_path(@member)
+    wines_myindex_path(resource)
   end
 
   def after_sign_out_path_for(resource)
